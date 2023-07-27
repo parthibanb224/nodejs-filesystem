76b1056f4d6946acd9925b16727273ba6d236385
parthibanb224 <parthibanb224@gmail.com>
2023-07-27 11:58:03 +0530

    port config

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ HTTP_SERVER.use(bodyParser.json());
 HTTP_SERVER.use(bodyParser.urlencoded({ extended: false }));
 
 const port = 5000;
-HTTP_SERVER.listen(port,'localhost',()=>{
+HTTP_SERVER.listen(port,'0.0.0.0',()=>{
     console.log("server start in this port",port);
 })
 
